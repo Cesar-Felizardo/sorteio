b5f42cf454e31d484946816e043977b04e721b7a
Cesar-Felizardo <70537620+Cesar-Felizardo@users.noreply.github.com>
2022-07-06 16:52:50 -0300

    atualização JS

--- script.js
+++ script.js
@@ -1,4 +1,82 @@
-const pessoas = {
-    p1: `Lucineide felizardo`, 126
+
+function sorteio(name, number) {
+    const pessoa1 = {
+        nome: 'Lucineide felizardo',
+        número: 126
+    }
+    
+    const pessoa2 = {
+        nome: 'Barbara',
+        número: 23
+    }
+    
+    const pessoa3 = {
+        nome: 'Barbara',
+        número: 04
+    }
+    
+    const pessoa4 = {
+        nome: 'Barbara',
+        número: 28
+    }
+    
+    const pessoa5 = {
+        nome: 'Barbara',
+        número: 14
+    }
+    
+    const pessoa6 = {
+        nome: 'Barbara',
+        número: 29
+    }
+    
+    const pessoa7 = {
+        nome: 'Thalles',
+        número: 8
+    }
+    
+    const pessoa8 = {
+        nome: 'Thalles',
+        número: 17
+    }
+    
+    const pessoa9 = {
+        nome: 'Luana',
+        número: 30
+    }
+    
+    const pessoa10 = {
+        nome: 'Judivan',
+        número: 13
+    }
+    
+    const pessoa11 = {
+        nome: 'Isac',
+        número: 33
+    }
+    
+    const pessoa12 = {
+        nome: 'Isal',
+        número: 05
+    }
+    
+    const pessoa13 = {
+        nome: 'Debora',
+        número: 74
+    }
+    
+    const pessoa14 = {
+        nome: 'Luan',
+        número: 51
+    }
+    const pessoa15 = {
+        nome: 'Milton',
+        número: 101
+    }
+    
+    const pessoa16 = {
+        nome: 'Vanessa',
+        número: 78
+    }
 }
 
