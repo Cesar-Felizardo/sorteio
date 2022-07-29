7915351a70f18c3278d82e9ec08a6ef542020f18
Cesar-Felizardo <70537620+Cesar-Felizardo@users.noreply.github.com>
2022-07-29 14:11:46 -0300

    29-07-2022

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const array_names = [
 
     array_names.push('Célia número (47)','Berg (sanitário) (72)',' Ênio (Pisos) (70)',
     'borges(145)','Berg (Laminados) (31)','Flávia (caixas) (56)',  'Edilene (caixas) (100)',
-     'Anderson (caixas) (38)', 'Marcelo (sanitário) (18)', 'Elaine sanitário (86)' )
+     'Anderson (caixas) (38)', 'Marcelo (sanitário) (18)', 'Elaine sanitário (86)','xandi sanitário (122)', 'Mira (99)' )
 
 let time1 = 0
 let time2 = 200
